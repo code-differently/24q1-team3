d70c124b9268547bcd3abb0d497d4c057a6bdc25
SherllinT <s.torres91@gmail.com>
2024-05-02 11:25:21 -0400

    feats: installed .env and updated server.js to reflect the change

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,43 +1,45 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+
 const app = express();
 const port = 5501;
 
-app.use(cors());
-
-
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-import('node-fetch').then(({ default: fetch }) => {
-    async function fetchRestaurantsFromYelp() {
-        try {
-            const response = await fetch('https://api.yelp.com/v3/businesses/search?location=New+York&categories=restaurants', {
-                headers: {
-                    'Authorization': 'Bearer E9aiE_f_KtvK2q2TUhA_WlZICoN-_Ba8bQ9hAhDSsc2Qf0MIewCvC8PeNOIVaVqNG5pi-yUKdIXOVWvR3VgdaEqddf-i65XLNQNL9jKPsmaaaqqeTr1N14v2v2cqZnYx'
-                }
-            });
-            if (!response.ok) {
-                throw new Error('Failed to fetch restaurants from Yelp API');
-            }
-            const data = await response.json();
-            return data.businesses;
-        } catch (error) {
-            console.error(error);
-            return [];
-        }
-    }
+const yelpApiKey = process.env.YELP_API_KEY;
 
-    app.get('/restaurants', async (req, res) => {
-        try {
-            const restaurants = await fetchRestaurantsFromYelp();
-            res.json(restaurants);
-        } catch (error) {
-            res.status(500).json({ error: 'Failed to fetch restaurant data' });
+
+app.use(cors());
+
+async function fetchRestaurantsFromYelp() {
+    try {
+        const response = await fetch('https://api.yelp.com/v3/businesses/search?location=New+York&categories=restaurants', {
+            headers: {
+                'Authorization': 'Bearer YOUR_YELP_API_KEY' 
+            }
+        });
+        if (!response.ok) {
+            throw new Error('Failed to fetch restaurants from Yelp API');
         }
-    });
-}).catch(error => {
-    console.error('Error importing node-fetch:', error);
+        const data = await response.json();
+        return data.businesses;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+app.get('/restaurants', async (req, res) => {
+    try {
+        const restaurants = await fetchRestaurantsFromYelp();
+        res.json(restaurants);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch restaurant data' });
+    }
 });
 
 app.listen(port, () => {
